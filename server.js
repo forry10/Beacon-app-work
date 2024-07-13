8c3b4f68bedc398e6c07ed2e000bb5b17c2eb1bd
Jack Forryan <jack.forryan@hotmail.com>
2024-07-13 12:52:31 +0100

    mongodb update

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/beaconSurvey', {
+// Update the connection URI if needed
+const mongoURI = 'mongodb://localhost:27017/beaconSurvey';
+
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+}).then(() => {
+    console.log('Successfully connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
 });
 
 const surveySchema = new mongoose.Schema({
