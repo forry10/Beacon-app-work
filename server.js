8787202c148af6e8884cbe8679ca7f873a532ed4
Jack Forryan <jack.forryan@hotmail.com>
2024-07-13 12:50:53 +0100

    use of mongoose

--- server.js
+++ server.js
@@ -35,24 +35,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/submit-survey', (req, res) => {
+app.post('/submit-survey', async (req, res) => {
     const formData = req.body;
 
     // Transform the data
     const transformedData = transformData(formData);
 
-    // Save to MongoDB
-    const newSurvey = new Survey(transformedData);
-    newSurvey.save((err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Failed to save survey data');
-        } else {
-            console.log('Survey data saved:', transformedData);
-            sendEmail(transformedData);
-            res.send('Survey submitted successfully');
-        }
-    });
+    try {
+        // Save to MongoDB
+        const newSurvey = new Survey(transformedData);
+        await newSurvey.save();
+        console.log('Survey data saved:', transformedData);
+
+        // Send email
+        await sendEmail(transformedData);
+
+        res.send('Survey submitted successfully');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Failed to save survey data');
+    }
 });
 
 function transformData(data) {
@@ -74,29 +76,24 @@ function transformData(data) {
     };
 }
 
-function sendEmail(data) {
+async function sendEmail(data) {
     // Configure the email transport using nodemailer
     let transporter = nodemailer.createTransport({
-        service: 'Gmail', // Replace with your email provider
+        service: 'Gmail',
         auth: {
-            user: 'jack.forry10@gmail.com', // Replace with your email
-            pass: 'aiwx sodx aict twhm' // Replace with your app password
+            user: 'jack.forry10@gmail.com', // Replace with your Gmail address
+            pass: 'aiwx sodx aict twhm' // Replace with your App Password
         }
     });
 
     let mailOptions = {
-        from: 'your-email@gmail.com',
+        from: 'jack.forry10@gmail.com',
         to: 'jack.forry10@gmail.com',
         subject: 'Survey Results',
         text: JSON.stringify(data, null, 2) // Convert data to a string for the email body
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Email sent: ' + info.response);
-    });
+    return transporter.sendMail(mailOptions);
 }
 
 app.listen(port, () => {
